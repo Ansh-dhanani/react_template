ba8cfcc4c4ae6b388733bbef517ccbb7a22b5ddb
Ansh-Dhanani <dhananiansh01@gmail.com>
2025-07-20 16:24:51 +0530

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+function App() {
+  /*
+"font-apple-garamond"
+"font-merapro"
+"font-boone"
+"font-default-lingo"    
+"font-minecraft"
+"font-montblanc"
+"font-palmore"
+"font-tan-kulture"
+*/
+
+// for colour variables use the following format:
+/**
+bg-[var(--color-bg)]
+text-[var(--color-text)]
+border-[var(--color-border)]
+hover:bg-[var(--color-secondary)]
+bg-[var(--color-primary)]
+*/
+
+
+  return (
+    <>
+       <h1>this is template</h1>
+    </>
+  );
+}
+
+export default App;
